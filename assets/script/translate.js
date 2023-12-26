d986037ef8f8e4529211b0c3603d7e43337203fc
AsdhqDdqsudWqhLjgbdvAabdDawqsdqFdLwHdq <154580107+AsdhqDdqsudWqhLjgbdvAabdDawqsdqFdLwHdq@users.noreply.github.com>
2023-12-26 12:52:59 +0500

    2

--- assets/script/translate.js
+++ assets/script/translate.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
-    var translations = {
-      en: {
-          guilds: 'Guilds',
-          meta: 'NoHello',
-      },
-      ru: {
-          guilds: 'Гильдии',
-          meta: 'Мета',
-      }
-    };    
-    var userLanguage = navigator.language || navigator.userLanguage;
-    var currentLanguage = translations[userLanguage.substr(0, 2)] ? userLanguage.substr(0, 2) : 'en';
-
-    translate(currentLanguage);
-
-    function translate(language) {
-      $('[data-translate]').each(function() {
-        var key = $(this).data('translate');
-        $(this).html(translations[language][key]);
-      });
+  var translations = {
+    en: {
+        home: 'Home',
+        guilds: 'Guilds',
+        meta: 'NoHello',
+    },
+    ru: {
+      home: 'Главная',
+      guilds: 'Гильдии',
+      meta: 'Мета',
     }
- });
+  };    
+  var userLanguage = navigator.language || navigator.userLanguage;
+  var currentLanguage = translations[userLanguage.substr(0, 2)] ? userLanguage.substr(0, 2) : 'en';
+
+  translate(currentLanguage);
+
+  function translate(language) {
+    $('[data-translate]').each(function() {
+      var key = $(this).data('translate');
+      $(this).html(translations[language][key]);
+    });
+  }
+});
